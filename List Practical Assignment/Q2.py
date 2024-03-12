d4b02eea8aa6720c0471fcde57cc518c65faaa07
shivansh-012 <www.shivansh065@gmail.com>
2024-03-13 01:36:46 +0530

    new files added

--- List Practical Assignment/Q2.py
+++ List Practical Assignment/Q2.py
@@ -1,6 +1,16 @@
-show_name=input("Enter name of show: ")
-start_time=input("Enter starting time(HH:MM:SS AM/PM): ")
-end_time=input("Enter ending time(HH:MM:SS AM/PM): ")
-a=len(show_name)
-print("{:<{}} {:<10} {}".format("NAME",a+3,"START_NAME","END_TIME" ))
-print("{:<{}} {:<10} {}".format(show_name, a+3, start_time, end_time))
+name_s = input("enter name of the station: ")
+day = input("enter day of the week: ")
+
+schedule_list = []
+n = int(input("no. of shows on that day: "))
+while n>0:
+    show_name = input("Enter name of show: ")
+    start_time = input("Enter starting time(HH:MM:SS AM/PM): ")
+    end_time = input("Enter ending time(HH:MM:SS AM/PM): ")
+    schedule_list.append([show_name,start_time,end_time])
+    n-=1
+
+print("TV Schedule for {} on {}:".format(name_s,day))
+print("{:<20} {:<15} {:<15}".format('Show Name', 'Start Time', 'Stop Time'))
+for ele in schedule_list:
+    print("{:<20} {:<15} {:<15}".format(ele[0], ele[1], ele[2]))
