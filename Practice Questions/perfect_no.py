6eaf543bb53120cd37752f0dfa688db849767021
shivansh-012 <www.shivansh065@gmail.com>
2024-03-19 01:58:20 +0530

    added

--- Practice Questions/perfect_no.py
+++ Practice Questions/perfect_no.py
@@ -1,8 +1,10 @@
 n=int(input())
-for i in range(1,n+1):
-    sum=0
-    for j in range(1,i):
-        if i%j==0:
-            sum+=j
-    if sum==i:
-        print(i)
+sum=0
+for i in range(1,n):
+    if n%i==0:
+        sum+=i
+
+if sum==n:
+    print(True)
+else:
+    print(False)    
